40754593c4b2fbfe0e323cd43b12c2eb7b76c899
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-12-05 01:08:38 +0100

    day4-part1: simplified

--- day04/src/Lib.hs
+++ day04/src/Lib.hs
@@ -3,14 +3,13 @@ module Lib
     ) where
 
 passwordsCount :: IO ()
-passwordsCount = print $ allAllComb - norepeat
+passwordsCount = print $ allCombinations - norepeat
 
-allAllComb = allComb 6 4 + (allComb 4 9 - allComb 4 4)
+allCombinations = combinationsCount (6 - 1) 9 4 + combinationsCount (4 - 1) 5 9
 
-allComb digitNumber till = pink (digitNumber - 1) 9 till
+norepeat = combinationsCount (6 - 1) 4 9
 
-pink 1 x _         = sum [1..x]
-pink counter x end = sum $ [ pink (counter - 1) n 9 | n <- [(10-end)..x] ]
+combinationsCount 1 start _         = sum [1..start]
+combinationsCount counter start end = sum $ [ combinationsCount (counter - 1) n 9 | n <- [(10-end)..start] ]
 
-norepeat = pink 5 4 9
-
+-- input = "10847-562041" -> "111111-559999" -> "111111-4999999" and "555555-559999"
