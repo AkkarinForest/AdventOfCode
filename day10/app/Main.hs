027c44a2555da76c61ca3f2585771de585566348
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-12-11 00:55:53 +0100

    day10-part2

--- day10/app/Main.hs
+++ day10/app/Main.hs
@@ -3,4 +3,6 @@ module Main where
 import           Lib
 
 main :: IO ()
-main = run
+main = do
+  run1
+  run2
--- day10/src/Lib.hs
+++ day10/src/Lib.hs
@@ -1,17 +1,42 @@
 module Lib
-    ( run
+    ( run1,
+    run2
     ) where
 
 
-run :: IO ()
-run = do
-  input <- lines <$> readFile "src/input.txt"
--- print $ (astrCoor input (1,0))
-  print $ findMax input
+import           Data.Sort
 
-findMax amap = maximum $ map (\point -> count amap point) $ astrCoor2 amap
+-- second star --
 
-count amap point = length $ removeShadowed (astrCoor amap point) []
+run2 :: IO ()
+run2 = do
+  astroidMap <- lines <$> readFile "src/input.txt"
+  let (_, bestPoint) = findMax astroidMap
+  let allVisible = sortOn comparison $ removeShadowed (asteroidCoordsRelative astroidMap bestPoint) []
+  let (x,y) = allVisible!!199
+  let (x', y') = bestPoint
+  print $ (x+x',y'-y)
+
+comparison (x,y)
+  | (x==0 && y>0) = (1,0)
+  | (x>0 && y>0) = (1, (fromIntegral x)/ (fromIntegral y))
+  | (x>0 && y==0) = (1, 9999999999999)
+  | (x>0 && y<0) = (2, (fromIntegral x)/ (fromIntegral y))
+  | (x==0 && y<0) = (2, 0)
+  | (x<0 && y<0) = (3, (fromIntegral x)/ (fromIntegral y))
+  | (x<0 && y==0) = (3, 9999999999999)
+  | (x<0 && y>0) = (4, (fromIntegral x)/ (fromIntegral y))
+
+-- first star --
+
+run1 :: IO ()
+run1 = do
+  astroidMap <- lines <$> readFile "src/input.txt"
+  print $ findMax astroidMap
+
+findMax astroidMap = maximum $ map (\point -> ((count astroidMap point),point)) $ asteroidCoords astroidMap
+
+count astroidMap point = length $ removeShadowed (asteroidCoordsRelative astroidMap point) []
 
 removeShadowed [x] acc = x:acc
 removeShadowed (x:xs) acc =
@@ -19,50 +44,31 @@ removeShadowed (x:xs) acc =
     True  -> removeShadowed xs acc
     False -> removeShadowed xs (x:acc)
     where shadowed = or $ [inOneLine x x' | x' <- acc]
-          res = [inOneLine x x' | x' <- acc]
 
 inOneLine::  (Int, Int) -> (Int, Int) -> Bool
-inOneLine (x, 0) (x',0)   = x*x' > 0
+inOneLine (x, 0) (x',0)   = x*x' >= 0
 inOneLine (_, 0) (_,_)    = False
 inOneLine (_, _) (_,0)    = False
-inOneLine (0, y) (0, y')  = y*y' > 0
 inOneLine (x, y) (x', y') = sameTangent && sameQuarter
                             where sameTangent = ((fromIntegral x)/ (fromIntegral y) == (fromIntegral x')/ (fromIntegral y'))
                                   sameQuarter = (x*x'>=0) && (y*y'>0)
 
 
-astrCoor:: [[Char]] -> (Int, Int) -> [(Int, Int)]
-astrCoor amap (cx, cy) =
-  let astrCoorInRowXY = astrCoorInRow (cx,cy)
-  in filter ((/=) (0,0)) $ concat $ map astrCoorInRowXY $ zip amap [0..]
+asteroidCoordsRelative:: [[Char]] -> (Int, Int) -> [(Int, Int)]
+asteroidCoordsRelative astroidMap (cx, cy) =
+  let asteroidCoordsRelativeInRowXY = asteroidCoordsRelativeInRow (cx,cy)
+  in filter ((/=) (0,0)) $ concat $ map asteroidCoordsRelativeInRowXY $ zip astroidMap [0..]
 
-astrCoorInRow :: (Int, Int) ->( [Char], Int) -> [(Int, Int)]
-astrCoorInRow (cx, cy) (line, y) =
+asteroidCoordsRelativeInRow :: (Int, Int) ->( [Char], Int) -> [(Int, Int)]
+asteroidCoordsRelativeInRow (cx, cy) (line, y) =
   [(x - cx, cy - y) | (x, obj) <- zip [0..] line, obj == '#' ]
 
 
-astrCoor2:: [[Char]] -> [(Int, Int)]
-astrCoor2 amap =
-  let astrCoorInRowXY = astrCoorInRow2
-  in filter ((/=) (0,0)) $ concat $ map astrCoorInRowXY $ zip amap [0..]
+asteroidCoords:: [[Char]] -> [(Int, Int)]
+asteroidCoords astroidMap =
+  let asteroidCoordsInRowXY = asteroidCoordsInRow
+  in filter ((/=) (0,0)) $ concat $ map asteroidCoordsInRowXY $ zip astroidMap [0..]
 
-astrCoorInRow2 :: ( [Char], Int) -> [(Int, Int)]
-astrCoorInRow2 (line, y) =
+asteroidCoordsInRow :: ( [Char], Int) -> [(Int, Int)]
+asteroidCoordsInRow (line, y) =
   [(x, y) | (x, obj) <- zip [0..] line, obj == '#' ]
-
-
-  -- removeShadowed [x] acc = x:acc
-  -- removeShadowed (x:xs) acc =
-  --   case shadowed of
-  --     True  -> removeShadowed xs acc
-  --     False -> removeShadowed xs ((x,res):acc)
-  --     where shadowed = or $ map (\(x,_) -> x) res
-  --           res = [inOneLine x x' | x' <- accc]
-  --           accc = map (\(x, _) -> x)  acc
-  --
-  -- -- inOneLine::  (Int, Int) -> (Int, Int) -> (Bool, Int, Int, Int)
-  -- inOneLine (x, 0) (x',0)   = (x*x' > 0, [1,x,0,x',0])
-  -- inOneLine (_, 0) (_,_)    = (False, [1,99,0,99,99])
-  -- inOneLine (_, _) (_,0)    = (False, [1,99,99,99,0])
-  -- inOneLine (0, y) (0, y')  = (y*y' > 0, [1,0,y,0,y'])
-  -- inOneLine (x, y) (x', y') = ((div x y == div x' y') && (x*x'>0) && (y*y'>0), [1,x,y,x',y'])
