c94eba9599e854c9c7b324a9bcc84adb59ae0b08
AkkarinForest <alexander.suminski@lunarlogic.io>
2019-12-21 09:28:31 +0100

    day14-part1: more wip

--- day14/src/Lib.hs
+++ day14/src/Lib.hs
@@ -26,38 +26,40 @@ toH input = format <$> S.splitOn " => " <$> L.init input
 calc hash (result, (nOfResult, ings)) = (nOfResult,yy)
   where yy = react hash <$> ings
 
-react hash ing =
-  case result of
-          -- "ORE"   -> 1
-          result' -> yellow nOfIng hash result'
-          -- result' -> n * (calc hash $ findReaction hash result')
-  where result = ing'!!1
-        nOfIng :: Int = read $ ing'!!0
-        ing' = S.splitOn " " ing
 
-yellow nOfResult hash result = (nn,yy)
-  where (nOfIng, yy) = calc2 hash (findReaction hash result)
-        nn = ceiling $ (fromIntegral nOfResult) / (fromIntegral nOfIng)
-        rest = nn - (div nOfResult nOfIng)
-
-calc2 hash (result, (nOfResult, ings)) = (nOfResult,yy)
-  where yy = react2 hash <$> ings
-
-react2 hash ing =
-  case result of
-          -- "ORE"   -> 1
-          result' -> yellow2 nOfIng hash result'
-          -- result' -> n * (calc hash $ findReaction hash result')
-  where result = ing'!!1
-        nOfIng :: Int = read $ ing'!!0
-        ing' = S.splitOn " " ing
-
--- yellow2 3 hash "A"
-yellow2 nOfResult hash result = (nn, ings)
-  where (nOfIng, ings) = nreaction
-        nreaction = (findReaction hash result)
-        nn = ceiling $ (fromIntegral nOfResult) / (fromIntegral nOfIng)
-        rest = nn - (div nOfResult nOfIng)
+--
+-- react hash ing =
+--   case result of
+--           -- "ORE"   -> 1
+--           result' -> yellow nOfIng hash result'
+--           -- result' -> n * (calc hash $ findReaction hash result')
+--   where result = ing'!!1
+--         nOfIng :: Int = read $ ing'!!0
+--         ing' = S.splitOn " " ing
+--
+-- yellow nOfResult hash result = (nn,yy)
+--   where (nOfIng, yy) = calc2 hash (findReaction hash result)
+--         nn = ceiling $ (fromIntegral nOfResult) / (fromIntegral nOfIng)
+--         rest = nn - (div nOfResult nOfIng)
+--
+-- calc2 hash (result, (nOfResult, ings)) = (nOfResult,yy)
+--   where yy = react2 hash <$> ings
+--
+-- react2 hash ing =
+--   case result of
+--           -- "ORE"   -> 1
+--           result' -> yellow2 nOfIng hash result'
+--           -- result' -> n * (calc hash $ findReaction hash result')
+--   where result = ing'!!1
+--         nOfIng :: Int = read $ ing'!!0
+--         ing' = S.splitOn " " ing
+--
+-- -- yellow2 3 hash "A"
+-- yellow2 nOfResult hash result = (nn, ings)
+--   where (nOfIng, ings) = nreaction
+--         nreaction = (findReaction hash result)
+--         nn = ceiling $ (fromIntegral nOfResult) / (fromIntegral nOfIng)
+--         rest = nn - (div nOfResult nOfIng)
 
 
 
